a7c27ba448ac6704bb82a5177023d56b26902647
giuliodondi <giuliodondi94@gmail.com>
2020-12-26 20:59:52 +0100

    added kernel utils + removed files

--- Assignment2/src/include/kernel_t.h
+++ Assignment2/src/include/kernel_t.h
@@ -11,14 +11,15 @@ typedef struct {
 } kernel_t;
 
 
+int8_t alloc_kernel( kernel_t* k, const unsigned int ker_s );
+int8_t copy_kernel(kernel_t* new_ker, kernel_t *old_ker) ;
+void delete_kernel( kernel_t* k) ;
 int8_t kernel_init(kernel_t* k, const unsigned int kernel_type, const unsigned int kernel_size, const float kernel_weight);
 void average_kernel(kernel_t* k);
 void weighted_kernel(kernel_t* k, const float kernel_weight);
 void gaussian_kernel_simple(kernel_t* k);
-//void gaussian_kernel(kernel_t* k);
 
-void get_kernel_normalisations(kernel_t* k);
+void kernel_normalisations(kernel_t* k);
 
-void delete_kernel( kernel_t* k) ;
 
 #endif
